bd23b2fc4cbedf5ed0841cfc9e3a1bfee5e8257b
Elton Pereira <elton_pereira@live.com>
2021-01-11 18:50:20 -0300

    PreLoad Done

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, LoadingIcon } from './styles'
+import AsyncStorage from '@react-native-community/async-storage'
+import { useNavigation } from '@react-navigation/native'
 
 import BarberLogo from '../../assets/barber.svg'
 
 export default () => {
+
+    const navigation = useNavigation()
+
+    useEffect(() => {
+        const checktoken = async () => {
+            const token = await AsyncStorage.getItem('token')
+            if (token) {
+                //validar o token
+            } else {
+                navigation.navigate('SignIn')
+
+            }
+
+        }
+        checktoken()
+
+    }, [])
+
+
+
     return (
         <Container>
             <BarberLogo width="100%" height="160" />
             <LoadingIcon size="large" color="#FFFFFF" />
-            
+
 
         </Container>
     )
