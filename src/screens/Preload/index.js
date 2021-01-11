5568c615dfa84074e72096e4407f02d9d74896a0
Elton Pereira <elton_pereira@live.com>
2021-01-11 18:30:36 -0300

    all fixed

---
+++ src/screens/Preload/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, LoadingIcon } from './styles'
+
+import BarberLogo from '../../assets/barber.svg'
+
+export default () => {
+    return (
+        <Container>
+            <BarberLogo width="100%" height="160" />
+            <LoadingIcon size="large" color="#FFFFFF" />
+            
+
+        </Container>
+    )
+}
