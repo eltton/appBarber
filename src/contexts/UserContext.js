519e99128cfaab83c4a40357d056559684b70086
Elton Pereira <elton_pereira@live.com>
2021-01-12 23:14:13 -0300

    screen login

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useReducer } from 'react'
+import { initialState, userReducer } from '../reducers/UserReducer'
+
+export const UserContext = createContext()
+
+export default ({ children }) => {
+    const [state, dispatch] = useReducer(userReducer, initialState)
+    return (
+        <UserContext.Provider value={{state, dispatch}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
